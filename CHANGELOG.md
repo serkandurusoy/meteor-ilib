f2c7b220548e787d336ecb94693e072c4cdf13b3
serkandurusoy <serkan.durusoy@dna-tr.com>
2015-05-05 11:17:01 +0300

    bumped npm version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,12 @@
+# 11.0.2_1
+
+May 05, 2015
+
+* Bump version of ilib which added support for new locales
+  and fixed the getAvailableLocales() function
+
 # 11.0.1_1
+
 May 03, 2015
 
 * Bump version of ilib which added support for new locales.
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'serkandurusoy:ilib',
-  version: '11.0.1_1',
+  version: '11.0.2_1',
   summary: 'Wrapper for iLib, a cross-engine library of internationalization (i18n) classes written in pure JS',
   git: 'https://github.com/serkandurusoy/meteor-ilib',
   documentation: 'README.md'
@@ -8,7 +8,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   Npm.depends({
-    ilib: "11.0.1"
+    ilib: "11.0.2"
   });
   api.versionsFrom('1.0');
   api.addFiles('ilib.js', 'server');
